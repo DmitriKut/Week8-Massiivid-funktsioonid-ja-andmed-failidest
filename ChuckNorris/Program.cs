c427c826e1b05984c31cb73bc1987f69df88b716
Dmitri <dimaskut@gmail.com>
2023-01-08 22:46:59 +0200

    controll flow

---
+++ ChuckNorris/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace ChuckNorris
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			
+				string filePath = @"C:\Users\...\samples\chuck.txt";
+				string[] dataFromFile = File.ReadAllLines(filePath);
+				string randomElement = dataFromFile[GenerateRandomIndex(dataFromFile)];
+
+				Console.WriteLine($"{randomElement}");
+
+		
+		}
+
+		private static int GenerateRandomIndex(string[] dataFromFile)
+		{
+			Random rnd = new Random();
+			int randomIndex = rnd.Next(0, dataFromFile.Length);
+			return randomIndex;
+		}
+	}
+}
---
+++ FridayNightRandom/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FridayNightRandom
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			DisplayRandomFood();
+			DisplayRandomDrink();
+			DisplayRandomMovie();
+		}
+		private static int GenerateRandomIndex(string[] sameArray)
+		{
+			Random rnd = new Random();
+			int randomIndex = rnd.Next(0, sameArray.Length);
+			return randomIndex;
+		}
+
+		private static void DisplayRandomFood()
+		{
+			string[] foods = { "chicken wings", "pizza", "sushi", "popocorn", "salad" };
+
+			Console.WriteLine($"Computer picked: {foods[GenerateRandomIndex(foods)]}");
+		}
+		private static void DisplayRandomDrink()
+		{
+			string[] drinks = { "apple juice", "coke", "fanta", "sprite" };
+
+			Console.WriteLine($"Computer picked: {drinks[GenerateRandomIndex(drinks)]}");
+		}
+		private static void DisplayRandomMovie()
+		{
+			string[] movies = { "Forest Gump", "Gladiator", "Avatar", "Perl Harbor" };
+
+			Console.WriteLine($"Computer picked: {movies[GenerateRandomIndex(movies)]}");
+		}
+	}
+}
